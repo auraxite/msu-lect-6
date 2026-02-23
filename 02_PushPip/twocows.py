71e1701f18665776e2ab92575dee0af93e4a03e9
Кирилл Кибизов <kokibizov@gmail.com>
2026-02-23 18:16:04 +0300

    fixed output

--- 02_PushPip/twocows.py
+++ 02_PushPip/twocows.py
@@ -3,7 +3,7 @@ import cowsay
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(prog="twocows.py")
+    parser = argparse.ArgumentParser()
 
     parser.add_argument("-f", dest="cow1", default="default")
     parser.add_argument("-e", dest="eyes1", default="oo")
@@ -15,9 +15,9 @@ def main() -> None:
 
     parser.add_argument("-W", dest="width", type=int, default=40)
     parser.add_argument("-n", dest="wrap_text", action="store_false")
-
     parser.add_argument("message1")
     parser.add_argument("message2")
+
     args = parser.parse_args()
 
     left = cowsay.cowsay(
@@ -36,9 +36,18 @@ def main() -> None:
         width=args.width,
         wrap_text=args.wrap_text,
     )
+    
+    left_lines = left.splitlines()
+    right_lines = right.splitlines()
+    h = max(len(left_lines), len(right_lines))
+    left_lines = ([""] * (h - len(left_lines))) + left_lines
+    right_lines = ([""] * (h - len(right_lines))) + right_lines
 
-    print(left)
-    print(right)
+    left_width = max((len(line) for line in left_lines))
+    out = []
+    for l, r in zip(left_lines, right_lines):
+        out.append(l.ljust(left_width) + " " + r)
+    print("\n".join(out))
 
 
 if __name__ == "__main__":
